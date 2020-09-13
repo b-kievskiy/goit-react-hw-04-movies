ba79ae6bcd7776cab3c9783c94b0ac30eb66a5a2
Bogdan <mormul96@ukr.net>
2020-09-13 12:00:44 +0300

    last

---
+++ build/precache-manifest.8b85288791cd9055139c08bc10b5b682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50412aa3d7dfbe328da30ea3f393e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7097643f70b4b2af21bb",
+    "url": "/static/css/main.276d74cf.chunk.css"
+  },
+  {
+    "revision": "8ba39f5871aae551a21d",
+    "url": "/static/js/2.e4b6d654.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e4b6d654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7097643f70b4b2af21bb",
+    "url": "/static/js/main.0a1a7869.chunk.js"
+  },
+  {
+    "revision": "7dcfc3fe069edf084e5c",
+    "url": "/static/js/runtime-main.c85cac17.js"
+  }
+]);
